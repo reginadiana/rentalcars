89c06bcc977d86f26bbad224fef643b2294ef0b2
Diana-ops <dianaregina22@outlook.com.br>
2020-04-27 00:57:35 -0300

    View Models

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
 	root to: 'home#index'
 	resources :car_categories, only: [:index, :show]
 	resources :manufacturers
+	resources :car_models, only: [:index, :show]
 end
---
+++ spec/features/admin_view_car_models_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+feature 'Admin view car models' do
+	scenario 'successfully' do
+		fiat = Manufacturer.create!(name: 'Fiat')
+		ford = Manufacturer.create!(name: 'Ford')
+		cat_a = CarCategory.create!(name: 'A', daily_rate: 50, car_insurance: 50, 							third_part_insurance: 30)
+
+		CarModel.create!(name: 'Uno', year: 2020, manufacturer: fiat,
+				motorization: '1.0', fuel_type: 'Flex', car_category: cat_a)
+		CarModel.create!(name: 'Ka', year: 2021, manufacturer: ford,
+				motorization: '1.0', fuel_type: 'Flex', car_category: cat_a)
+
+		visit root_path
+		click_on 'Modelo de Carros'
+		
+		expect(page).to have_content 'Fiat'
+		expect(page).to have_content 'Uno'
+		expect(page).to have_content '2020'
+
+		expect(page).to have_content 'Ford'
+		expect(page).to have_content 'Ka'
+		expect(page).to have_content '2021'
+	end
+
+	scenario 'and view' do
+		fiat = Manufacturer.create!(name: 'Fiat')
+		cat_a = CarCategory.create!(name: 'A', daily_rate: 50, car_insurance: 50, 							third_part_insurance: 30)
+
+		CarModel.create!(name: 'Uno', year: 2020, manufacturer: fiat,
+					motorization: '1.0', fuel_type: 'Flex', car_category: cat_a)
+
+
+		visit root_path
+		click_on 'Modelo de Carros'
+		#click_on 'Ver detalhes'
+		find("a#details-#{cat_a.id}").click()
+
+		expect(page).to have_content 'Uno'
+		expect(page).to have_content 'Fabricante: Fiat'
+		expect(page).to have_content 'Ano: 2020'
+		expect(page).to have_content 'Motor: 1.0'
+		expect(page).to have_content 'Combustivel: Flex'
+		expect(page).to have_content 'Categoria: A'
+		expect(page).to have_content 'Diaria: $50.00'
+	end
+end
