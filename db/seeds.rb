359b9d469aa6ccdb05a38bfee9196ce84d7efae2
Diana-ops <dianaregina22@outlook.com.br>
2020-04-29 12:39:45 -0300

    Populando banco de dados no seeds para manufacturers, car_category, car_models e customers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Manufacturers (Fabricantes Default)
+
+volkswagem = Manufacturer.create!(name: 'Volkswagen')
+mercedes = Manufacturer.create!(name: 'Mercedes')
+
+# Car Category (Categorias de Carro Default)
+car_category_a = CarCategory.create!(name: 'A', daily_rate: 100, car_insurance: 100, third_part_insurance: 100)
+car_category_b = CarCategory.create!(name: 'B', daily_rate: 80, car_insurance: 70, third_part_insurance: 150)
+
+# Customers (Clientes Default)
+Customer.create!(name: 'Lucas', document: '508.218.249.15', email: 'lucas@gmail.com')
+Customer.create!(name: 'Diana', document: '408.018.249.90', email: 'diana@gmail.com')
+
+# Car models (Modelos de Carro Default)
+CarModel.create!(name: 'Ferrari', year: 1955, manufacturer: volkswagem,
+					motorization: '5.0', fuel_type: 'Flex', car_category: car_category_a)
+
+CarModel.create!(name: 'Porchi', year: 2000, manufacturer: mercedes,
+					motorization: '1.0', fuel_type: 'Flex', car_category: car_category_b)
