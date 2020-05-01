bfe926dd8f487e077732377527637e5b6f8cfcb4
Diana-ops <dianaregina22@outlook.com.br>
2020-05-01 16:59:05 -0300

    Escrevendo teste para cadastro de carros (cars) para frota e I18n de cars

---
+++ spec/features/car/admin_register_car_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+feature 'Admin register valid car' do
+  scenario 'and license_plate must be unique' do
+    fiat = Manufacturer.create!(name: 'Fiat')
+    cat_a = CarCategory.create!(name: 'A', daily_rate: 50, car_insurance: 50, 							third_part_insurance: 30)
+
+    car_model = CarModel.create!(name: 'Uno', year: 2020, manufacturer: fiat,
+					motorization: '1.0', fuel_type: 'Flex', car_category: cat_a)
+
+    subsidiary = Subsidiary.create!(name: 'ACCENTURE', cnpj: '99.168.496/0001-74', address: 'Rua: Paulista')
+
+    Car.create!(license_plate: '2508ABC', color: 'Pink', car_model: car_model, mileage: 500, subsidiary: subsidiary)
+
+    visit root_path
+    click_on 'Carros das Frotas'
+    click_on 'Registrar novo carro para a frota'
+
+    fill_in 'Placa', with: '2508ABC'
+    click_on 'Enviar'
+
+    expect(page).to have_content('Placa deve ser único')
+  end
+
+  scenario 'and license_plate can not be blank' do
+    visit root_path
+    click_on 'Carros das Frotas'
+    click_on 'Registrar novo carro para a frota'
+
+    fill_in 'Placa', with: ''
+    click_on 'Enviar'
+
+    expect(page).to have_content('Placa não pode ficar em branco')
+  end
+
+ scenario 'and return to list cars' do
+
+    visit root_path
+    click_on 'Carros das Frotas'
+    click_on 'Registrar novo carro para a frota'
+    click_on 'Voltar'
+
+    expect(current_path).to eq cars_path
+  end
+
+end
+
