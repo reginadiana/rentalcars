5db636413c35390c0dcf4085e17232fa57ff6812
Diana-ops <dianaregina22@outlook.com.br>
2020-05-05 19:12:17 -0300

    Populacao de seeds com novos dados e teste para view de rentals

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,8 +16,8 @@ car_category_a = CarCategory.create!(name: 'A', daily_rate: 100, car_insurance: 
 car_category_b = CarCategory.create!(name: 'B', daily_rate: 80, car_insurance: 70, third_part_insurance: 150)
 
 # Customers (Clientes Default)
-Customer.create!(name: 'Lucas', document: '508.218.249.15', email: 'lucas@gmail.com')
-Customer.create!(name: 'Diana', document: '408.018.249.90', email: 'diana@gmail.com')
+customer_a = Customer.create!(name: 'Lucas', document: '508.218.249.15', email: 'lucas@gmail.com')
+customer_b = Customer.create!(name: 'Diana', document: '408.018.249.90', email: 'diana@gmail.com')
 
 # Car models (Modelos de Carro Default)
 car_model = CarModel.create!(name: 'Ferrari', year: 1955, manufacturer: volkswagem,
@@ -30,6 +30,27 @@ CarModel.create!(name: 'Porchi', year: 2000, manufacturer: mercedes,
 subsidiary_a = Subsidiary.create!(name: 'Hold', cnpj: '35.463.771/0001-08', address: 'Rua: Paulista')
 subsidiary_b = Subsidiary.create!(name: 'Pics', cnpj: '38.533.249/0001-35', address: 'Rua: Mocca')
 
-#Cars (Carros Default)
+# Cars (Carros Default)
 Car.create!(license_plate: '2508ABD', color: 'Pink', car_model: car_model, mileage: 500, subsidiary: subsidiary_a)
 Car.create!(license_plate: '2708ABD', color: 'Black', car_model: car_model, mileage: 100, subsidiary: subsidiary_b)
+
+# Subsidiaries (Locações Default)
+
+Rental.create!(start_date: 1.day.from_now, end_date: 2.day.from_now, customer: customer_a, car_category: car_category_a)
+Rental.create!(start_date: 1.day.from_now, end_date: 2.day.from_now, customer: customer_b, car_category: car_category_b)
+
+# Admin (Users Default)
+User.create!(email: 'dianaregina22@outlook.com.br', password: '123456')
+User.create!(email: 'dianaregina22@yahoo.com', password: '111111')
+
+
+
+
+
+
+
+
+
+
+
+
--- spec/features/rentals/user_view_rental_spec.rb
+++ spec/features/rentals/user_view_rental_spec.rb
@@ -1,6 +1,43 @@
 require 'rails_helper'
 
 feature 'User view rental' do
-  xscenario 'successfully' do
-  end
+	scenario 'successfully' do
+		customer = Customer.create!(name: 'Diana', document: '505.450.444-17', email: 'diana@gmail.com')
+		car_category = CarCategory.create!(name: 'A', daily_rate: 100, car_insurance: 100, third_part_insurance: 100)
+		user = User.create!(email: 'joana@teste.com.br', password: '12345678')
+
+		rental = Rental.create!(start_date: 1.day.from_now, end_date: 2.day.from_now, customer: customer, car_category: car_category)
+
+		login_as user, scope: :user
+
+		visit root_path
+		click_on 'Locações'
+
+		expect(page).to have_content("Data de inicio: #{rental.start_date}")
+		expect(page).to have_content("Data de término: #{rental.end_date}")
+		expect(page).to have_content('Cliente: Diana')
+		expect(page).to have_content('Categoria: A')
+	end
+
+	scenario 'and no rentals are created' do
+		user = User.create!(email: 'diana@teste.com.br', password: '12345678')
+		login_as user, scope: :user
+
+		visit root_path
+		click_on 'Locações'
+
+		expect(page).to have_content('Nenhuma locação cadastrada')
+	end
+
+	scenario 'and return to home page' do
+
+		user = User.create!(email: 'teste@teste.com.br', password: '12345678')
+		login_as user, scope: :user
+
+		visit root_path
+		click_on 'Locações'
+		click_on 'Voltar'
+
+		expect(current_path).to eq root_path
+	end
 end
