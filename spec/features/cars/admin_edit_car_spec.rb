a7ca8283feeb41e7eb826223aa9a4dd1b28d73a8
Diana-ops <dianaregina22@outlook.com.br>
2020-05-05 22:09:21 -0300

    Escrevendo teste para editar cars e cria-lo com sucesso

--- spec/features/cars/admin_edit_car_spec.rb
+++ spec/features/cars/admin_edit_car_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'Visitor view cars' do
+feature 'Admin edits car' do
 	scenario 'successfully' do
 		fiat = Manufacturer.create!(name: 'Fiat')
 		cat_a = CarCategory.create!(name: 'A', daily_rate: 50, car_insurance: 50, 							third_part_insurance: 30)
@@ -10,19 +10,21 @@ feature 'Visitor view cars' do
 
 		subsidiary = Subsidiary.create!(name: 'ACCENTURE', cnpj: '99.168.496/0001-74', address: 'Rua: Paulista')
 		Car.create!(license_plate: '2508ABC', color: 'Pink', car_model: car_model, mileage: 500, subsidiary: subsidiary)
-		Car.create!(license_plate: '1819ABC', color: 'Pink', car_model: car_model, mileage: 500, subsidiary: subsidiary)
-    
+
 		user = User.create!(email: 'teste@teste.com.br', password: '12345678')
 		login_as user, scope: :user
 
 		visit root_path
 		click_on 'Carros das Frotas'
+		click_on '2508ABC'
+		click_on 'Editar'
+		fill_in 'Placa', with: '252525'
+		click_on 'Enviar'
 
-		expect(page).to have_content('2508ABC')
-		expect(page).to have_content('1819ABC')
+		expect(page).to have_content('252525')
 	end
 
-	scenario 'and view details' do
+	scenario 'can not be blank' do
 		fiat = Manufacturer.create!(name: 'Fiat')
 		cat_a = CarCategory.create!(name: 'A', daily_rate: 50, car_insurance: 50, 							third_part_insurance: 30)
 
@@ -30,7 +32,6 @@ feature 'Visitor view cars' do
 					motorization: '1.0', fuel_type: 'Flex', car_category: cat_a)
 
 		subsidiary = Subsidiary.create!(name: 'ACCENTURE', cnpj: '99.168.496/0001-74', address: 'Rua: Paulista')
-
 		Car.create!(license_plate: '2508ABC', color: 'Pink', car_model: car_model, mileage: 500, subsidiary: subsidiary)
 
 		user = User.create!(email: 'teste@teste.com.br', password: '12345678')
@@ -39,37 +40,20 @@ feature 'Visitor view cars' do
 		visit root_path
 		click_on 'Carros das Frotas'
 		click_on '2508ABC'
+		click_on 'Editar'
 
-		expect(page).to have_content('Placa: 2508ABC')
-		expect(page).to have_content('Cor: Pink')
-		expect(page).to have_link('Uno', href: car_model_path(car_model)) 
-		expect(page).to have_content('Quilometragem: 500')
-		expect(page).to have_link('ACCENTURE', href: subsidiary_path(subsidiary)) 
+		fill_in 'Placa', with: ''
+		fill_in 'Cor', with: ''
+		fill_in 'Quilometragem', with: ''
 
-	end
-	scenario 'and no cars are created' do
-		user = User.create!(email: 'teste@teste.com.br', password: '12345678')
-		login_as user, scope: :user
+		click_on 'Enviar'
 
-		visit root_path
-		click_on 'Carros das Frotas'
-
-		expect(page).to have_content('Nenhum carro cadastrado')
+		expect(page).to have_content('Placa não pode ficar em branco')
+		expect(page).to have_content('Cor não pode ficar em branco')
+		expect(page).to have_content('Quilometragem não pode ficar em branco')
 	end
 
-	scenario 'and return to home page' do
-		user = User.create!(email: 'teste@teste.com.br', password: '12345678')
-		login_as user, scope: :user
-	
-		visit root_path
-		click_on 'Carros das Frotas'
-		click_on 'Voltar'
-
-		expect(current_path).to eq root_path
-	end
-
-	scenario 'and return to cars page' do
-
+	scenario 'must be unique' do
 		fiat = Manufacturer.create!(name: 'Fiat')
 		cat_a = CarCategory.create!(name: 'A', daily_rate: 50, car_insurance: 50, 							third_part_insurance: 30)
 
@@ -77,8 +61,9 @@ feature 'Visitor view cars' do
 					motorization: '1.0', fuel_type: 'Flex', car_category: cat_a)
 
 		subsidiary = Subsidiary.create!(name: 'ACCENTURE', cnpj: '99.168.496/0001-74', address: 'Rua: Paulista')
-
 		Car.create!(license_plate: '2508ABC', color: 'Pink', car_model: car_model, mileage: 500, subsidiary: subsidiary)
+
+		Car.create!(license_plate: 'AAAAAA', color: 'Pink', car_model: car_model, mileage: 500, subsidiary: subsidiary)
 
 		user = User.create!(email: 'teste@teste.com.br', password: '12345678')
 		login_as user, scope: :user
@@ -86,6 +71,32 @@ feature 'Visitor view cars' do
 		visit root_path
 		click_on 'Carros das Frotas'
 		click_on '2508ABC'
+		click_on 'Editar'
+		
+		fill_in 'Placa', with: 'AAAAAA'
+
+		click_on 'Enviar'
+
+		expect(page).to have_content('Placa já está em uso')
+	end
+
+	scenario 'and return to list car categories' do
+		fiat = Manufacturer.create!(name: 'Fiat')
+		cat_a = CarCategory.create!(name: 'A', daily_rate: 50, car_insurance: 50, 							third_part_insurance: 30)
+
+		car_model = CarModel.create!(name: 'Uno', year: 2020, manufacturer: fiat,
+					motorization: '1.0', fuel_type: 'Flex', car_category: cat_a)
+
+		subsidiary = Subsidiary.create!(name: 'ACCENTURE', cnpj: '99.168.496/0001-74', address: 'Rua: Paulista')
+		Car.create!(license_plate: '2508ABC', color: 'Pink', car_model: car_model, mileage: 500, subsidiary: subsidiary)
+		user = User.create!(email: 'teste@teste.com.br', password: '12345678')
+		login_as user, scope: :user
+
+		visit root_path
+		click_on 'Carros das Frotas'
+		click_on '2508ABC'
+		click_on 'Editar'
+
 		click_on 'Voltar'
 
 		expect(current_path).to eq cars_path
--- spec/features/cars/admin_register_car_spec.rb
+++ spec/features/cars/admin_register_car_spec.rb
@@ -1,69 +1,93 @@
 require 'rails_helper'
 
 feature 'Admin register valid car' do
-  scenario 'and license_plate must be unique' do
-    fiat = Manufacturer.create!(name: 'Fiat')
-    cat_a = CarCategory.create!(name: 'A', daily_rate: 50, car_insurance: 50, 							third_part_insurance: 30)
-
-    car_model = CarModel.create!(name: 'Uno', year: 2020, manufacturer: fiat,
+	scenario 'successfully' do
+		fiat = Manufacturer.create!(name: 'Fiat')
+		cat_a = CarCategory.create!(name: 'A', daily_rate: 50, car_insurance: 50, 							third_part_insurance: 30)
+		car_model = CarModel.create!(name: 'Uno', year: 2020, manufacturer: fiat,
 					motorization: '1.0', fuel_type: 'Flex', car_category: cat_a)
 
-    subsidiary = Subsidiary.create!(name: 'ACCENTURE', cnpj: '99.168.496/0001-74', address: 'Rua: Paulista')
+		subsidiary = Subsidiary.create!(name: 'ACCENTURE', cnpj: '99.168.496/0001-74', address: 'Rua: Paulista')
 
-    Car.create!(license_plate: '0000AAA', color: 'Pink', car_model: car_model, mileage: 500, subsidiary: subsidiary)
+		user = User.create!(email: 'teste@teste.com.br', password: '12345678')
+		login_as user, scope: :user
 
-    user = User.create!(email: 'teste@teste.com.br', password: '12345678')
-    login_as user, scope: :user
+		visit root_path
+		click_on 'Carros das Frotas'
+		click_on 'Registrar novo carro para a frota'
 
-    visit root_path
-    click_on 'Carros das Frotas'
-    click_on 'Registrar novo carro para a frota'
+		fill_in 'Placa', with: '0000AAA'
+		fill_in 'Cor', with: 'Black'
+		select 'Uno', from: 'Modelo de Carro'	
+		fill_in 'Quilometragem', with: '1'
+		select 'ACCENTURE', from: 'Filial'
 
-    fill_in 'Placa', with: '0000AAA'
-    click_on 'Enviar'
+		click_on 'Enviar'
 
-    expect(page).to have_content('Placa já está em uso')
-  end
+		expect(page).to have_content('0000AAA')
+	end
+	scenario 'and license_plate must be unique' do
+		fiat = Manufacturer.create!(name: 'Fiat')
+		cat_a = CarCategory.create!(name: 'A', daily_rate: 50, car_insurance: 50, 							third_part_insurance: 30)
 
-  scenario 'lenght of mileage must be bigger then 0' do
-    user = User.create!(email: 'teste@teste.com.br', password: '12345678')
-    login_as user, scope: :user
+		car_model = CarModel.create!(name: 'Uno', year: 2020, manufacturer: fiat,
+					motorization: '1.0', fuel_type: 'Flex', car_category: cat_a)
 
-    visit root_path
-    click_on 'Carros das Frotas'
-    click_on 'Registrar novo carro para a frota'
+		subsidiary = Subsidiary.create!(name: 'ACCENTURE', cnpj: '99.168.496/0001-74', address: 'Rua: Paulista')
 
-    fill_in 'Quilometragem', with: '-34'
-    click_on 'Enviar'
+		Car.create!(license_plate: '0000AAA', color: 'Pink', car_model: car_model, mileage: 500, subsidiary: subsidiary)
 
-    expect(page).to have_content('Quilometragem deve ser maior que 0')
-  end
+		user = User.create!(email: 'teste@teste.com.br', password: '12345678')
+		login_as user, scope: :user
 
-  scenario 'and license_plate can not be blank' do
-    user = User.create!(email: 'teste@teste.com.br', password: '12345678')
-    login_as user, scope: :user
+		visit root_path
+		click_on 'Carros das Frotas'
+		click_on 'Registrar novo carro para a frota'
 
-    visit root_path
-    click_on 'Carros das Frotas'
-    click_on 'Registrar novo carro para a frota'
+		fill_in 'Placa', with: '0000AAA'
+		click_on 'Enviar'
 
-    fill_in 'Placa', with: ''
-    click_on 'Enviar'
+		expect(page).to have_content('Placa já está em uso')
+	end
 
-    expect(page).to have_content('Placa não pode ficar em branco')
-  end
+	scenario 'lenght of mileage must be bigger then 0' do
+		user = User.create!(email: 'teste@teste.com.br', password: '12345678')
+		login_as user, scope: :user
 
- scenario 'and return to list cars' do
-    user = User.create!(email: 'teste@teste.com.br', password: '12345678')
-    login_as user, scope: :user
+		visit root_path
+		click_on 'Carros das Frotas'
+		click_on 'Registrar novo carro para a frota'
 
-    visit root_path
-    click_on 'Carros das Frotas'
-    click_on 'Registrar novo carro para a frota'
-    click_on 'Voltar'
+		fill_in 'Quilometragem', with: '-34'
+		click_on 'Enviar'
 
-    expect(current_path).to eq cars_path
-  end
+		expect(page).to have_content('Quilometragem deve ser maior que 0')
+	end
 
+	scenario 'and license_plate can not be blank' do
+		user = User.create!(email: 'teste@teste.com.br', password: '12345678')
+		login_as user, scope: :user
+
+		visit root_path
+		click_on 'Carros das Frotas'
+		click_on 'Registrar novo carro para a frota'
+
+		fill_in 'Placa', with: ''
+		click_on 'Enviar'
+
+		expect(page).to have_content('Placa não pode ficar em branco')
+	end
+
+	scenario 'and return to list cars' do
+		user = User.create!(email: 'teste@teste.com.br', password: '12345678')
+		login_as user, scope: :user
+
+		visit root_path
+		click_on 'Carros das Frotas'
+		click_on 'Registrar novo carro para a frota'
+		click_on 'Voltar'
+
+		expect(current_path).to eq cars_path
+	end
 end
 
