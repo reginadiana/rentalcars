45beb3bb2aa7807a09c0280e013c5ca47e46b9e4
Diana-ops <dianaregina22@outlook.com.br>
2020-05-08 16:04:38 -0300

    Teste para verificar presenca do menu na home page

--- spec/features/visitor_open_home_page_spec.rb
+++ spec/features/visitor_open_home_page_spec.rb
@@ -1,14 +1,27 @@
 require 'rails_helper'
 
 feature 'Visitor open home page' do
-  scenario 'successfully' do
+	scenario 'successfully' do
 
-    user = User.create!(email: 'teste@teste.com.br', password: '12345678')
-    login_as user, scope: :user
+		user = User.create!(email: 'teste@teste.com.br', password: '12345678')
+		login_as user, scope: :user
 
-    visit root_path
+		visit root_path
 
-    expect(page).to have_content('Rental Cars')
-    expect(page).to have_content('Bem vindo ao sistema de gestão de locação')
-  end
+		expect(page).to have_content('Rental Cars')
+		expect(page).to have_content('Bem vindo ao sistema de gestão de locação')
+
+		expect(page).to have_link 'Home'
+		expect(page).to have_link 'Clientes'
+		expect(page).to have_link 'Fabricantes'
+		expect(page).to have_link 'Locações'
+		expect(page).to have_link 'Filiais'
+		expect(page).to have_link 'Modelos de Carros'
+		expect(page).to have_link 'Categorias de Carros'
+		expect(page).to have_link 'Carros das Frotas'
+		expect(page).to have_link 'Sair'
+
+		expect(page).not_to have_link 'Entrar'
+
+	end
 end
