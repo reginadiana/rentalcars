c97bcd328e17a150d243c7e6cb252789b0c82da7
Diana-ops <dianaregina22@outlook.com.br>
2020-05-09 14:07:10 -0300

    Adicionando partials para customers com i18n

--- spec/features/customers/user_view_customer_spec.rb
+++ spec/features/customers/user_view_customer_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 feature 'User view customer' do
 	scenario 'successfully' do
-		Customer.create!(name: 'Lucas', document: '508.218.249.15', email: 'lucas@gmail.com')
-		Customer.create!(name: 'Sara', document: '452.176.388-00', email: 'sara@gmail.com')
+		customer_a = Customer.create!(name: 'Lucas', document: '508.218.249.15', email: 'lucas@gmail.com')
+		customer_b = Customer.create!(name: 'Sara', document: '452.176.388-00', email: 'sara@gmail.com')
 
 		user = User.create!(email: 'teste@teste.com.br', password: '12345678')
 		login_as user, scope: :user
@@ -11,12 +11,22 @@ feature 'User view customer' do
 		visit root_path
 		click_on 'Clientes'
 
+		expect(page).to have_content('Cliente')
+		expect(page).to have_content('Email')
+		expect(page).to have_content('CPF')
+
 		expect(page).to have_content('Lucas')
 		expect(page).to have_content('508.218.249.15')
 		expect(page).to have_content('lucas@gmail.com')
 		expect(page).to have_content('Sara')
 		expect(page).to have_content('452.176.388-00')
 		expect(page).to have_content('sara@gmail.com')
+
+		expect(page).to have_link "edit-#{customer_a.id}" 
+		expect(page).to have_link "delete-#{customer_a.id}"
+
+		expect(page).to have_link "edit-#{customer_b.id}" 
+		expect(page).to have_link "delete-#{customer_b.id}"
 	end
 
 	scenario 'and no custumers are created' do
