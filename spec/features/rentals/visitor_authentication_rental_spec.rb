7c910b871e9c3fbe59e5986206540f6b17b67da8
Diana-ops <dianaregina22@outlook.com.br>
2020-05-11 14:45:34 -0300

    Refatorando testes para autenticacao de visitantes para fabricantes e filiais

---
+++ spec/features/rentals/visitor_authentication_rental_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+feature 'Visitor tries to acess rental and' do
+
+	after :each do
+		expect(current_path).to eq(new_user_session_path)
+		expect(page).to have_content('Para continuar, efetue login ou registre-se.')
+	end
+
+  	scenario 'cannot view index unless logged in' do
+    		visit root_path
+
+    		expect(page).not_to have_link('Locações')
+  	end
+  	scenario 'cannot view rental index unless logged in' do
+    		visit rentals_path
+  	end
+
+        scenario 'and must be authenticated to see detals' do
+		rental = create(:rental)
+		visit rental_path(rental)	
+   	end
+
+	scenario 'and must be authenticated to create a new rental' do
+		visit new_rental_path	
+	end
+        scenario 'and must be authenticated to edit some rental' do
+		rental = create(:rental)
+
+		visit edit_rental_path(rental)
+   	end
+end
